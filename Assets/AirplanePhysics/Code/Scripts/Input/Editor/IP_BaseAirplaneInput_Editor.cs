52cd7bb1dc8f702d17acb5239f2232861f2ccb8f
Hasi-b <hasib.hasan9@gmail.com>
2021-05-04 17:30:53 +0600

    Custom editor for Input class
    Added a custom input for Keyboard Input class to monitor the values

--- Assets/AirplanePhysics/Code/Scripts/Input/Editor/IP_BaseAirplaneInput_Editor.cs
+++ Assets/AirplanePhysics/Code/Scripts/Input/Editor/IP_BaseAirplaneInput_Editor.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class IP_BaseAirplaneInput_Editor : MonoBehaviour
+
+namespace Qubitech
 {
-    // Start is called before the first frame update
-    void Start()
+    [CustomEditor(typeof(IP_Base_Airplane_Input))]
+    public class IP_BaseAirplaneInput_Editor : Editor
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        #region variable
+
+        private IP_Base_Airplane_Input targetInput;
+
+
+        #endregion
+
+        #region builtin Methods
+
+        private void OnEnable()
+        {
+            targetInput = (IP_Base_Airplane_Input)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            string debugInfo = "";
+            debugInfo += "Pitch: " + targetInput.Pitch + "\n";
+            debugInfo += "Roll: " + targetInput.Roll + "\n";
+            debugInfo += "Yaw: " + targetInput.Yaw + "\n";
+            debugInfo += "Throttle: " + targetInput.Throttle + "\n";
+            debugInfo += "Brake: " + targetInput.Brake + "\n";
+            debugInfo += "flap: " + targetInput.Flap + "\n";
+            //custom editor code
+            GUILayout.Space(20);
+            EditorGUILayout.TextArea(debugInfo,GUILayout.Height(200));
+            GUILayout.Space(20);
+            Repaint();
+
+        }
+
+
+        #endregion
     }
 }
